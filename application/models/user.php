81cc2531f496c33b4adf74061c328ba684dfdac2
faizanayubi <indianayubi@gmail.com>
2015-06-18 15:51:46 +0530

    updated User Model

--- application/models/user.php
+++ application/models/user.php
@@ -10,42 +10,22 @@ class User extends Shared\Model {
     /**
      * @column
      * @readwrite
-     * @primary
-     * @type autonumber
+     * @type text
+     * @length 255
+     * 
+     * @validate required, alpha, min(3), max(255)
+     * @label full name
      */
-    protected $_id;
+    protected $_name;
 
     /**
      * @column
      * @readwrite
      * @type text
-     * @length 100
+     * @length 255
      * 
-     * @validate required, alpha, min(3), max(32)
-     * @label first name
-     */
-    protected $_first;
-
-    /**
-     * @column
-     * @readwrite
-     * @type text
-     * @length 100
-     * 
-     * @validate required, alpha, min(3), max(32)
-     * @label last name
-     */
-    protected $_last;
-
-    /**
-     * @column
-     * @readwrite
-     * @type text
-     * @length 100
-     * @index
-     * 
-     * @validate required, max(100)
-     * @label email address
+     * @validate alpha, min(3), max(255)
+     * @label email
      */
     protected $_email;
 
@@ -56,16 +36,21 @@ class User extends Shared\Model {
      * @length 100
      * @index
      * 
-     * @validate required, alpha, min(8), max(32)
+     * @validate required, max(15)
+     * @label phone number
+     */
+    protected $_phone;
+
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 100
+     * @index
+     * 
+     * @validate required, alpha, min(8), max(255)
      * @label password
      */
     protected $_password;
     
-    /**
-    * @column
-    * @readwrite
-    * @type boolean
-    */
-    protected $_admin = false;
-
 }
