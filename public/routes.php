75011739b504d6fabe9b9bd474fc56d6e6fbb674
faizanayubi <indianayubi@gmail.com>
2015-06-18 19:28:22 +0530

    Added basic Routes

--- public/routes.php
+++ public/routes.php
@@ -7,6 +7,31 @@ $routes = array(
         "pattern" => "login",
         "controller" => "main",
         "action" => "login"
+    ),
+    array(
+        "pattern" => "about",
+        "controller" => "home",
+        "action" => "about"
+    ),
+    array(
+        "pattern" => "terms",
+        "controller" => "home",
+        "action" => "terms"
+    ),
+    array(
+        "pattern" => "privacy",
+        "controller" => "home",
+        "action" => "privacy"
+    ),
+    array(
+        "pattern" => "contact",
+        "controller" => "home",
+        "action" => "contact"
+    ),
+    array(
+        "pattern" => "faq",
+        "controller" => "home",
+        "action" => "faq"
     )
 );
 
