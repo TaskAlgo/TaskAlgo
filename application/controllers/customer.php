a8e7d8b7a18c5213618e1c00ccd673ae08873116
faizanayubi <indianayubi@gmail.com>
2015-06-18 17:07:17 +0530

    Added Register Method

--- application/controllers/customer.php
+++ application/controllers/customer.php
@@ -6,11 +6,21 @@
  * @author Faizan Ayubi
  */
 use Framework\Registry as Registry;
+use Framework\RequestMethods as RequestMethods;
 
 class Customer extends Main {
 
     public function register() {
-        
+        if (RequestMethods::post("action") == "addCustomer") {
+            $user = new User(array(
+                "name" => RequestMethods::post("name"),
+                "email" => RequestMethods::post("email"),
+                "phone" => RequestMethods::post("phone"),
+                "password" => sha1(RequestMethods::post("password"))
+            ));
+            $user->save();
+            
+        }
     }
 
 }
