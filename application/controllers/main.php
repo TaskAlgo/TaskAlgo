ba87ead7b5676b54419453b0683fe9fcd179e79d
faizanayubi <indianayubi@gmail.com>
2015-06-19 18:56:40 +0530

    Added Login Logout Method

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -10,8 +10,33 @@ use Framework\Registry as Registry;
 
 class Main extends Controller {
 
-    public function login() {
+    public function login($ep = NULL, $password = NULL) {
+        if (isset($ep)) {
+            if(strpos($ep, "@")){
+                $user = User::first(array("email = ?" => $ep, "password = ?" => sha1($password)));
+            } else {
+                $user = User::first(array("phone = ?" => $ep, "password = ?" => sha1($password)));
+            }
+        }
         
+        if(Framework\RequestMethods::post("login")){
+            $ep = Framework\RequestMethods::post("ep");
+            $password = Framework\RequestMethods::post("password");
+            if(strpos($ep, "@")){
+                $user = User::first(array("email = ?" => $ep, "password = ?" => sha1($password)));
+            } else {
+                $user = User::first(array("phone = ?" => $ep, "password = ?" => sha1($password)));
+            }
+        }
+        
+        if(isset($user)){
+            $this->user = $info["user"];
+        }
+    }
+    
+    public function logout() {
+        $this->setUser(false);
+        self::redirect("/home");
     }
 
     /**
