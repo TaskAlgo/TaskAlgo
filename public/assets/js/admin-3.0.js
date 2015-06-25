4890e8f5bcaf61e9f9735802613529085078e405
faizanayubi <indianayubi@gmail.com>
2015-06-25 18:48:18 +0530

    Admin Module Started

---
+++ public/assets/js/admin-3.0.js
@@ -0,0 +1,118 @@
+(function (window, Model) {
+    window.request = Model.initialize();
+    window.opts = {};
+}(window, window.Model));
+
+$(function () {
+    $('#side-menu').metisMenu();
+});
+
+$(function () {
+    $('select[value]').each(function () {
+        $(this).val(this.getAttribute("value"));
+    });
+});
+
+//Loads the correct sidebar on window load
+$(function () {
+    $(window).bind("load", function () {
+        console.log($(this).width())
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse')
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse')
+        }
+    })
+});
+
+//Collapses the sidebar on window resize
+$(function () {
+    $(window).bind("resize", function () {
+        console.log($(this).width())
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse')
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse')
+        }
+    })
+});
+
+
+$(document).ready(function () {
+
+    //initialize beautiful datetime picker
+    $("input[type=date]").datepicker();
+    $("input[type=date]").datepicker("option", "dateFormat", "yy-mm-dd");
+
+    $('#created_stats').submit(function (e) {
+        $('#stats').html('<p class="text-center"><i class="fa fa-spinner fa-spin fa-5x"></i></p>');
+        e.preventDefault();
+        var data = $(this).serializeArray();
+        request.read({
+            action: "admin/data",
+            data: data,
+            callback: function (data) {
+                $('#stats').html('');
+                if (data.data) {
+                    Morris.Bar({
+                        element: 'stats',
+                        data: toArray(data.data),
+                        xkey: 'y',
+                        ykeys: ['a'],
+                        labels: ['Total']
+                    });
+                }
+            }
+        });
+    });
+    
+    $('#getstats').submit(function (e) {
+        $('#stats').html('<p class="text-center"><i class="fa fa-spinner fa-spin fa-5x"></i></p>');
+        e.preventDefault();
+        var data = $(this).serializeArray();
+        request.read({
+            action: "admin/stats",
+            data: data,
+            callback: function (data) {
+                $('#stats').html('');
+                if (data.data) {
+                    Morris.Bar({
+                        element: 'stats',
+                        data: toArray(data.data),
+                        xkey: 'y',
+                        ykeys: ['a'],
+                        labels: ['Total']
+                    });
+                }
+            }
+        });
+    });
+    
+    $('button[name=message]').click(function (e) {
+        var self = this;
+        window.opts.subject = $(this).data("subject");
+        window.opts.email = $(this).data("from");
+        $('#message_modal').modal('show');
+    });
+
+    $('#messageform').submit(function(e) {
+        e.preventDefault();
+        var body = $('#body').val();
+        request.create({
+            action: "employer/messages",
+            data: {action: 'support', subject: window.opts.subject, email: window.opts.email, body: body},
+            callback: function (data) {
+                $('#status').html('Message Sent Successfully!!!');
+                $('#message_modal').modal('hide');
+            }
+        });
+    });
+
+});
+
+function toArray(object) {
+    var array = $.map(object, function (value, index) {
+        return [value];
+    });
+    return array;
+}
