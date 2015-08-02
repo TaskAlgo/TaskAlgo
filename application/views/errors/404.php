cd7556f12b59fe772ba8a1e9466d78849b04a0b6
MERAJ AHMAD SIDDIQUI <merajsiddiqui@outlook.com>
2015-08-02 18:47:22 +0530

    Error 404
    Modification of Page 404 error

--- application/views/errors/404.php
+++ application/views/errors/404.php
@@ -1,12 +1,237 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="en">
+
     <head>
-        <title>SwiftMVC Framework</title>
+
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta http-equiv="Content-Type" content="text/html">
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Revisit-after" content="1 Day">
+
+
+
+        <title>TaskSphere 404</title>
+
+
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+
+
+        <link href="http://www.tasksphere.com/public/assets/css/style.css" rel="stylesheet">
+
+
+
+        <link href="http://assets.tasksphere.com/css/owl.carousel.css" rel="stylesheet">
+
+        <link href="http://assets.tasksphere.com/css/owl.theme.css" rel="stylesheet">
+
+
+
+
+
+        <!--[if lt IE 9]>
+
+              <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+
+              <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+
+        <![endif]-->
+
+
+
+        <!-- fav and touch icons -->
+
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://assets.tasksphere.com/ico/apple-touch-icon-144-precomposed.png">
+
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://assets.tasksphere.com/ico/apple-touch-icon-114-precomposed.png">
+
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://assets.tasksphere.com/ico/apple-touch-icon-72-precomposed.png">
+
+        <link rel="apple-touch-icon-precomposed" href="http://assets.tasksphere.com/ico/apple-touch-icon-57-precomposed.html">
+
+        <link rel="shortcut icon" href="http://assets.tasksphere.com/ico/favicon.png">
+
+        <script>paceOptions = {elements: true};</script>
+
+        <script src="http://assets.tasksphere.com/js/pace.min.js"></script>
+
     </head>
+
     <body>
-        error 404
-        <?php if (DEBUG): ?>
-            <pre><?php print_r($e); ?></pre>
-        <?php endif; ?>
+
+        <div id="wrapper">
+<div class="header">
+
+    <nav class="navbar   navbar-site navbar-default" role="navigation">
+
+        <div class="container">
+
+            <div class="navbar-header">
+
+                <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+
+                <a href="/" class="navbar-brand logo logo-title">
+
+                    <span class="logo-icon"><img src="http://www.tasksphere.com/public/assets/images/logo.jpg" width="50px" height="50px" /> </span> Task<span>Sphere </span><sup><small>beta</small></sup>
+
+                </a>
+
+            </div>
+
+            <div class="navbar-collapse collapse">
+
+                <?php if($user){?>
+
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li><a href="/users/logout">Signout <i class="glyphicon glyphicon-off"></i></a></li>
+
+                    <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <span><?php echo $user->name;?></span> <i class="icon-user fa"></i> <i class=" icon-down-open-big fa"></i></a>
+
+                        <ul class="dropdown-menu user-menu">
+
+                            <li class="active"><a href="account-home.html"><i class="icon-home"></i> Personal Home </a></li>
+
+                            <li><a href="account-myads.html"><i class="icon-th-thumb"></i> My Orders </a></li>
+                            <li><a href="account-saved-search.html"><i class="icon-star-circled"></i> Saved search </a></li>
+
+                            <li><a href="account-archived-ads.html"><i class="icon-folder-close"></i> Write a testimonial </a></li>
+
+                            <li><a href="statements.html"><i class=" icon-money "></i> Payment history </a></li>
+
+                        </ul>
+
+                    </li>
+
+                    <li class="postadd"><a class="btn btn-block   btn-border btn-post btn-danger" href="/bookService">Book Service</a></li>
+
+                </ul>
+
+                <?php }
+
+                else{
+                    ?>
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li><a href="/login">Login</a></li>
+
+                    <li><a href="/customers/register">Signup</a></li>
+
+                    <li class="postadd"><a class="btn btn-block btn-border btn-post btn-danger" href="/taskers/register">Become a Tasker</a></li>
+
+                </ul>
+
+                <?php }?>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+</div>
+
+<div class="main-container">
+
+    <div class="container">
+
+
+            
+            <div class="intro-inner">
+<div class="about-intro" style="
+    background:url('http://www.tasksphere.com/public/assets/images/error-image.png') no-repeat center;
+	background-size:cover;">
+<div class="dtable hw100">
+<div class="dtable-cell hw100">
+<div class="container text-center">
+    <h3 class="intro-title animated fadeInDown"><a href="/home">Go To Home</a> </h3>
+</div>
+</div>
+</div>
+</div>
+          
+            
+            
+        </div>
+
+
+
+    </div>
+
+</div>
+ 
+
+
+            <div class="footer" id="footer">
+
+                <div class="container">
+
+                    <ul class=" pull-left navbar-link footer-nav">
+
+                        <li><a href="/"> Home </a> <a href="/about"> About us </a> <a href="/terms"> Terms and Conditions </a> <a href="/privacy"> Privacy Policy </a> <a href="/contact"> Contact us </a> <a href="/faq"> FAQ </a>
+                        <span  align="center" class="social-list"><a target="_blank" href="https://twitter.com/"><i class="fa fa-twitter fa-lg "></i></a>
+                        <a target="_blank" href="https://www.facebook.com/tasksphere"><i class="fa fa-facebook fa-lg "></i></a>
+                        <a target="_blank" href="https://plus.google.com/"><i class="fa fa-google-plus fa-lg"></i></a>
+                        <a target="_blank" href="https://www.pinterest.com/tasksphere/"><i class="fa fa-pinterest fa-lg "></i></a>
+                    </span>
+                        </li>
+                    </ul>
+                    <ul class=" pull-right navbar-link footer-nav">
+
+                        <li>&copy;<a href="http://swiftdeal.org" target="_blank">SwiftDeal Online LLP</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+
+
+        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+
+
+        <script src="http://assets.tasksphere.com/js/owl.carousel.min.js"></script>
+
+
+
+        <script src="http://assets.tasksphere.com/js/jquery.matchHeight-min.js"></script>
+
+
+
+        <script src="http://assets.tasksphere.com/js/hideMaxListItem.js"></script>
+
+
+
+        <script src="http://assets.tasksphere.com/plugins/jquery.fs.scroller/jquery.fs.scroller.js"></script>
+
+        <script src="http://assets.tasksphere.com/plugins/jquery.fs.selecter/jquery.fs.selecter.js"></script>
+
+
+
+        <script src="http://assets.tasksphere.com/js/public.js"></script>
+
     </body>
+
 </html>
