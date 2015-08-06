085c6d6e1db2498700320ea3d791647ec62d9ed4
MERAJ AHMAD SIDDIQUI <merajsiddiqui@outlook.com>
2015-08-06 16:56:40 +0530

    Update
    Method

--- application/controllers/customers.php
+++ application/controllers/customers.php
@@ -43,7 +43,7 @@ class Customers extends Users {
     }
 
     public function payments() {
-
+        $this->changeLayout();
         $this->seo(array(
 
             "title" => "Happy Customers",
@@ -55,14 +55,15 @@ class Customers extends Users {
             "view" => $this->getLayoutView()
 
         ));
+        $view = $this->getActionView();
         // Merchant key here as provided by Payu
 $MERCHANT_KEY = "JBZaLc";
-
+$txn = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
 // Merchant Salt as provided by Payu
 $SALT = "GQs7yium";
 // End point - change to https://secure.payu.in for LIVE mode
 $PAYU_BASE_URL = "https://test.payu.in";
-
+        $view->set("txnid", $txn);
         $view->set("key", $MERCHANT_KEY);
         $view->set("salt", $SALT);
         $view->set("hash", $Hash);
