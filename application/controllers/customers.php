b5a7eb5fa55f0c57bb96ef484ac29464c8c684d3
MERAJ AHMAD SIDDIQUI <merajsiddiqui@outlook.com>
2015-08-07 17:27:37 +0530

    Payment Success
    Payment success measures

--- application/controllers/customers.php
+++ application/controllers/customers.php
@@ -20,6 +20,12 @@ use Framework\RequestMethods as RequestMethods;
 
 class Customers extends Users {
 
+    // Merchant Salt as provided by Payu
+// Merchant key here as provided by Payu
+public $MERCHANT_KEY = "2u0rmd";
+
+// Merchant Salt as provided by Payu
+public $SALT = "6zteiBW7";
     
 
     public function index() {
@@ -63,16 +69,10 @@ class Customers extends Users {
         $view = $this->getActionView();
         // Merchant key here as provided by Payu
 $txn = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
-// Merchant Salt as provided by Payu
-// Merchant key here as provided by Payu
-$MERCHANT_KEY = "2u0rmd";
-
-// Merchant Salt as provided by Payu
-$SALT = "6zteiBW7";
-$amount =1;
+$amount =500;
 $service = "Service take By Tasksphere";
 $user = $this->user;
-$posted = ["key"=>$MERCHANT_KEY,"txnid"=>$txn, "amount"=>$amount, "productinfo"=>$service, "firstname"=>$user->name, "email"=>$user->email];
+$posted = ["key"=>$this->MERCHANT_KEY,"txnid"=>$txn, "amount"=>$amount, "productinfo"=>$service, "firstname"=>$user->name, "email"=>$user->email];
     $hashSequence = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
     $hashVarsSeq = explode('|', $hashSequence);
     $hash_string = '';	
@@ -80,17 +80,16 @@ $posted = ["key"=>$MERCHANT_KEY,"txnid"=>$txn, "amount"=>$amount, "productinfo"=
       $hash_string .= isset($posted[$hash_var]) ? $posted[$hash_var] : '';
       $hash_string .= '|';
     }
-    $hash_string .= $SALT;
+    $hash_string .= $this->SALT;
     $hash = strtolower(hash('sha512', $hash_string));           
         $view->set("txnid", $txn);
-        $view->set("key", $MERCHANT_KEY);
-        $view->set("salt", $SALT);
+        $view->set("key", $this->MERCHANT_KEY);
         $view->set("hash", $hash);
 
     }
 
     public function payment_success() {
-
+        $this->changeLayout();
         $this->seo(array(
 
             "title" => "Happy Customers",
@@ -102,11 +101,19 @@ $posted = ["key"=>$MERCHANT_KEY,"txnid"=>$txn, "amount"=>$amount, "productinfo"=
             "view" => $this->getLayoutView()
 
         ));
-
         
-
+        if(RequestMethods::post("status")=="success")
+        {
+           $payu_unique_id= RequestMethods::post("mihpayid");
+           $name = RequestMethods::post("firstname");
+           $payment_mode = RequestMethods::post("mode");
+           $txnid = RequestMethods::post("txnid");
+           $amount = RequestMethods::post("amount");
+           $key = RequestMethods::post("key");
+           $payment_id = RequestMethods::post("payuMoneyId");
+           $other = RequestMethods::post("unmappedstatus");
+        }
         $view = $this->getActionView();
-
     }
 
     public function payment_failed() {
