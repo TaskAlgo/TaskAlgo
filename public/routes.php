6606b6cd7b4f1380ead21a9d385e38acc06178d6
MERAJ AHMAD SIDDIQUI <merajsiddiqui@outlook.com>
2015-08-10 23:18:14 +0530

    Blog
    Blog Route defined

--- public/routes.php
+++ public/routes.php
@@ -1,49 +1,1 @@
-<?php
-
-// define routes
-
-$routes = array(
-    array(
-        "pattern" => "login",
-        "controller" => "users",
-        "action" => "login"
-    ),
-    array(
-        "pattern" => "about",
-        "controller" => "home",
-        "action" => "about"
-    ),
-    array(
-        "pattern" => "terms",
-        "controller" => "home",
-        "action" => "terms"
-    ),
-    array(
-        "pattern" => "privacy",
-        "controller" => "home",
-        "action" => "privacy"
-    ),
-    array(
-        "pattern" => "contact",
-        "controller" => "home",
-        "action" => "contact"
-    ),
-    array(
-        "pattern" => "faq",
-        "controller" => "home",
-        "action" => "faq"
-    ),
-    array(
-        "pattern" => "bookService",
-        "controller" => "users",
-        "action" => "bookService"
-    )
-);
-
-// add defined routes
-foreach ($routes as $route) {
-    $router->addRoute(new Framework\Router\Route\Simple($route));
-}
-
-// unset globals
-unset($routes);
+<?php// define routes$routes = array(    array(        "pattern" => "login",        "controller" => "users",        "action" => "login"    ),    array(        "pattern" => "blog",        "controller" => "home",        "action" => "blog"    ),    array(        "pattern" => "about",        "controller" => "home",        "action" => "about"    ),    array(        "pattern" => "terms",        "controller" => "home",        "action" => "terms"    ),    array(        "pattern" => "privacy",        "controller" => "home",        "action" => "privacy"    ),    array(        "pattern" => "contact",        "controller" => "home",        "action" => "contact"    ),    array(        "pattern" => "faq",        "controller" => "home",        "action" => "faq"    ),    array(        "pattern" => "bookService",        "controller" => "users",        "action" => "bookService"    ));// add defined routesforeach ($routes as $route) {    $router->addRoute(new Framework\Router\Route\Simple($route));}// unset globalsunset($routes);
